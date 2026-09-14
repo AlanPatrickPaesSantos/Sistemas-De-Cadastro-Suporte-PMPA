2cede49c5bda33e731cd5f229df8a26892435f95
AlanPatrickPaesSantos <alanpatricktheking@gmail.com>
2026-09-14 14:46:18 -0300

    fix: torna patrimonio opcional no cadastro em lote

--- backend/services/servicoLoteService.test.js
+++ backend/services/servicoLoteService.test.js
@@ -6,6 +6,7 @@ const item = (overrides = {}) => ({ dataEnt: '2026-09-14', unidade: '15 BPM', te
 
 test('normaliza espaços e caixa para comparação', () => assert.equal(normalize('  AbC  '), 'abc'));
 test('aceita lote completo', () => assert.doesNotThrow(() => validateItems([item(), item({ rp: '124', nSerie: 'ABD' })])));
+test('aceita patrimônio ausente no lote', () => assert.doesNotThrow(() => validateItems([item({ nSerie: '' })])));
 test('bloqueia RP repetido', () => assert.throws(() => validateItems([item(), item({ nSerie: 'ABD' })]), /RP repetido/));
 test('bloqueia patrimônio repetido', () => assert.throws(() => validateItems([item(), item({ rp: '124' })]), /Patrimônio repetido/));
 test('bloqueia campos vazios', () => assert.throws(() => validateItems([item({ rp: '  ' })]), /campo obrigatório/));
