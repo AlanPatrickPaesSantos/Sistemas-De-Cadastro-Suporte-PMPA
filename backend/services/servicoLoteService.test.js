f8a63434b4f6d3e197640cae54e9604ba85ebca1
AlanPatrickPaesSantos <alanpatricktheking@gmail.com>
2026-09-13 23:41:45 -0300

    test: cobre validação do cadastro em lote

---
+++ backend/services/servicoLoteService.test.js
@@ -0,0 +1,11 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const { normalize, validateItems } = require('./servicoLoteService');
+
+const item = (overrides = {}) => ({ dataEnt: '2026-09-14', unidade: '15 BPM', tecnico: 'ALAN', tEquipSuporte: 'COMPUTADOR', analiseTecnica: 'ok', servico: 'LAUDO', rp: '123', nSerie: 'ABC', ...overrides });
+
+test('normaliza espaços e caixa para comparação', () => assert.equal(normalize('  AbC  '), 'abc'));
+test('aceita lote completo', () => assert.doesNotThrow(() => validateItems([item(), item({ rp: '124', nSerie: 'ABD' })])));
+test('bloqueia RP repetido', () => assert.throws(() => validateItems([item(), item({ nSerie: 'ABD' })]), /RP repetido/));
+test('bloqueia patrimônio repetido', () => assert.throws(() => validateItems([item(), item({ rp: '124' })]), /Patrimônio repetido/));
+test('bloqueia campos vazios', () => assert.throws(() => validateItems([item({ rp: '  ' })]), /campo obrigatório/));
