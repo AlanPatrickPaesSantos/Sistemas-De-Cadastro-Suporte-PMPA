f4955a252c1c081132e5004940dd2cdc1c087e3a
AlanPatrickPaesSantos <alanpatricktheking@gmail.com>
2026-09-14 14:56:59 -0300

    fix: permite reutilizar RP em novas ordens

--- backend/services/servicoLoteService.test.js
+++ backend/services/servicoLoteService.test.js
@@ -7,6 +7,6 @@ const item = (overrides = {}) => ({ dataEnt: '2026-09-14', unidade: '15 BPM', te
 test('normaliza espaços e caixa para comparação', () => assert.equal(normalize('  AbC  '), 'abc'));
 test('aceita lote completo', () => assert.doesNotThrow(() => validateItems([item(), item({ rp: '124', nSerie: 'ABD' })])));
 test('aceita patrimônio ausente no lote', () => assert.doesNotThrow(() => validateItems([item({ nSerie: '' })])));
-test('bloqueia RP repetido', () => assert.throws(() => validateItems([item(), item({ nSerie: 'ABD' })]), /RP repetido/));
+test('aceita RP repetido em novo atendimento', () => assert.doesNotThrow(() => validateItems([item(), item({ nSerie: 'ABD' })])));
 test('bloqueia patrimônio repetido', () => assert.throws(() => validateItems([item(), item({ rp: '124' })]), /Patrimônio repetido/));
 test('bloqueia campos vazios', () => assert.throws(() => validateItems([item({ rp: '  ' })]), /campo obrigatório/));
