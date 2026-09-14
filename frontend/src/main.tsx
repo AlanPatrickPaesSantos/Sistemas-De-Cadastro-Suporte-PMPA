ed1d09f8b266d2ed932fc3078046664ff253fb73
AlanPatrickPaesSantos <alanpatricktheking@gmail.com>
2026-09-14 00:46:05 -0300

    chore: tipa respostas e corrige interceptador

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -18,7 +18,8 @@ console.log(
 // Qualquer chamada 'fetch' feita pelo sistema passará por aqui primeiro para anexar o token
 const originalFetch = window.fetch;
 window.fetch = async (...args) => {
-  const [resource, config] = args;
+  const [resource, initialConfig] = args;
+  let config = initialConfig;
   
   const isApiRequest = typeof resource === 'string' && (
     resource.startsWith(API_BASE) || 
